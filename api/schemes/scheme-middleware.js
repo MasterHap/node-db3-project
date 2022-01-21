f75ea2f06bacd7fa1511d8918311b8bbe74d42a2
MasterHap <76802578+MasterHap@users.noreply.github.com>
2022-01-21 04:38:40 -0500

    middleware done

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,4 +1,4 @@
-const db = require('../../data/db-config')
+const db = require("../../data/db-config");
 /*
   If `scheme_id` does not exist in the database:
 
@@ -9,22 +9,22 @@ const db = require('../../data/db-config')
 */
 const checkSchemeId = async (req, res, next) => {
   try {
-    const existing = await db('schemes')
-      .where('scheme_id', req.params.scheme_id)
-      .first()
+    const existing = await db("schemes")
+      .where("scheme_id", req.params.scheme_id)
+      .first();
 
     if (!existing) {
       next({
-        status: 404, 
-        message: `scheme with scheme_id ${req.params.scheme_id} not found`
-      })
+        status: 404,
+        message: `scheme with scheme_id ${req.params.scheme_id} not found`,
+      });
     } else {
-      next()
+      next();
     }
   } catch (err) {
-    next(err)
+    next(err);
   }
-}
+};
 
 /*
   If `scheme_name` is missing, empty string or not a string:
@@ -35,8 +35,17 @@ const checkSchemeId = async (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-
-}
+  const { scheme_name } = req.body;
+  if (
+    scheme_name === undefined ||
+    typeof scheme_name !== "string" ||
+    !scheme_name.trim()
+  ) {
+    next({ status: 400, message: "invalid scheme_name" });
+  } else {
+    next();
+  }
+};
 
 /*
   If `instructions` is missing, empty string or not a string, or
@@ -48,11 +57,24 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
+  const { instructions, step_number } = req.body;
 
-}
+  if (
+    instructions === undefined ||
+    typeof instructions !== "string" ||
+    !instructions.trim() ||
+    typeof step_number !== "number" ||
+    step_number < 1
+  ) {
+    const error = { status: 400, message: "invalid step" };
+    next(error);
+  } else {
+    next();
+  }
+};
 
 module.exports = {
   checkSchemeId,
   validateScheme,
   validateStep,
-}
+};
